0ce9c9fc5fde700ef44f68a456cc96977401777f
slowdivePTG <1600012139@pku.edu.cn>
2020-03-01 11:03:43 +0800

    Task3 (1-body)

--- Galactic_Dynamics/TwoBody.f90
+++ Galactic_Dynamics/TwoBody.f90
@@ -3,14 +3,11 @@ program Two_Body
   use Func
   use Global
   implicit none
-  !real*8, parameter :: RL = a*(1/(1+q) + x)
-  !real*8, parameter :: RL1 = a*(q/(1+q) - x)
-  !real*8, parameter :: Rc = (1+q)*RL1**4/a**3
-  !Randomly generated
-  !Other initial parameters
-  real*8 :: theta, phi, thetac
+  !initial velocity
+  real*8 :: v10, v20, theta1, phi1, theta2, phi2
+  real*8 :: thetac
   !model parameters
-  real*8 :: v0, t, dt, a
+  real*8 :: t, dt, a
   !Mass
   real*8 :: m1, m2, q
   !velocity & location
@@ -21,32 +18,36 @@ program Two_Body
   !output header
   open(unit=71, file="./data_task2/R_V")
   open(unit=72, file="./data_task2/J")
+  open(unit=73, file="./data_task2/Para")
 
-  q = 1.d-1
-  v0 = 0.8
-  theta = pi/3.
-  phi = pi/6.
+  q = 2.d-1
+  v10 = 1.d0
+  theta1 = pi/3.
+  phi1 = pi/3.
+  v20 = 1.d-1
+  theta2 = pi/4.
+  phi2 = -pi/12.
 
   !Initial condition
   !!Mass & Radius
-  m1 = q / (1 + q)
-  m2 = 1 / (1 + q)
+  m1 = q / (1. + q)
+  m2 = 1. / (1. + q)
 
   !!Position
-  r1(1) = -1. / (1 + q)
+  r1(1) = -1. / (1. + q)
   r1(2) = 0.
   r1(3) = 0.
-  r2(1) = q * 1. / (1 + q)
+  r2(1) = q / (1. + q)
   r2(2) = 0.
   r2(3) = 0.
 
   !!Orbital velocity
-  v1(1) = v0*sin(theta)*cos(phi)
-  v1(2) = v0*sin(theta)*sin(phi)
-  v1(3) = v0*cos(theta)
-  v2(1) = 0.
-  v2(2) = 0.
-  v2(3) = 1.
+  v1(1) = v10*sin(theta1)*cos(phi1)
+  v1(2) = v10*sin(theta1)*sin(phi1)
+  v1(3) = v10*cos(theta1)
+  v2(1) = v20*sin(theta2)*cos(phi2)
+  v2(2) = v20*sin(theta2)*sin(phi2)
+  v2(3) = v20*cos(theta2)
 
   j1 = m1*cross_product_3d(r1, v1)
   j2 = m2*cross_product_3d(r2, v2)
@@ -56,6 +57,8 @@ program Two_Body
   write(71,'(999E22.12)') t, r1, r2, v1, v2
   write(72,'(a)') '#Time, J1, J2, J'
   write(72,'(999E22.12)') t, j1, j2
+  write(73,'(a)') '#q, v10, v20, theta1, phi1, theta2, phi2'
+  write(73,'(999E22.12)') q, v10, v20, theta1, phi1, theta2, phi2
 
   i = 0
   thetac = 0 !Stop when one orbit completes
@@ -93,5 +96,6 @@ program Two_Body
     
   close(unit=71)
   close(unit=72)
+  close(unit=73)
 
 end program Two_Body
